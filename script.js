75b5d7e67ce5eff43673c0e71107312a9c8f2d52
Burnett Miao <burnett@pcschool.com.tw>
2024-02-27 14:14:30 +0800

    header section finish

--- script.js
+++ script.js
@@ -13,3 +13,44 @@ navLinks.addEventListener('click', (e) => {
   navLinks.classList.remove('open');
   menuBtnIcon.setAttribute('class', 'ri-menu-line');
 });
+
+const headerImage = document.querySelector('.header__image');
+headerImage.addEventListener(
+  'animationend',
+  (e) => {
+    setTimeout(() => {
+      headerImage.classList.add('reveal');
+    });
+  },
+  { once: true }
+);
+
+const scrollRevealOption = {
+  distance: '50px',
+  origin: 'bottom',
+  duration: 1000,
+};
+
+ScrollReveal().reveal('.header__content h1', {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal('.header__content h2', {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+ScrollReveal().reveal('.header__content p', {
+  ...scrollRevealOption,
+  delay: 2500,
+});
+
+ScrollReveal().reveal('.header__content div', {
+  ...scrollRevealOption,
+  delay: 3000,
+});
+
+ScrollReveal().reveal('.header .nav__links', {
+  delay: 3500,
+});
